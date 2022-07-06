ba75bd33404f34546dcd14ef62dcd786c0ea3301
Lioshin21 <makivskii99@gmail.com>
2022-07-06 16:36:07 +0300

    Create TabNavigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Basket from "./components/Basket";
+import Products from "./components/Products";
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-    </View>
+    <>
+      <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Products" component={Products} />
+            <Tab.Screen name="Basket" component={Basket} />
+          </Tab.Navigator>
+        </NavigationContainer>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+
 });
 
 export default App;
